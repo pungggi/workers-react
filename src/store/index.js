f02331ff3b3a0cd76cb76a9d0fd6d672c16a29df
ap4147 <alessandro.pungitore@amanninformatik.ch>
2019-05-31 16:48:13 +0200

    add state persistence
    Signed-off-by: ap4147 <alessandro.pungitore@amanninformatik.ch>

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,14 @@
 import createStore from "pure-store"
+import defaultData from "./default"
 
-const $ = createStore({ count: 1 })
+const STORAGE_KEY = "__store___"
+
+const storedData = JSON.parse(localStorage.getItem(STORAGE_KEY))
+
+const $ = createStore(storedData || defaultData)
+
+window.addEventListener("beforeunload", () => {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify($.state))
+})
+
 export default $
