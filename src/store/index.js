884d652a2b6d71e61309c6c7661a08111749d93b
pungggi <alessandro.pungitore@gmail.com>
2019-06-01 12:40:00 +0000

    refactore persistence logic

--- src/store/index.js
+++ src/store/index.js
@@ -5,14 +5,11 @@ const STORAGE_KEY = "__store___"
 
 let storedData
 if (typeof document !== "undefined") {
-  storedData = JSON.parse(localStorage.getItem(STORAGE_KEY))
-}
-const $ = createStore(storedData || modelData)
-
-if (typeof document !== "undefined") {
   window.addEventListener("beforeunload", () => {
     localStorage.setItem(STORAGE_KEY, JSON.stringify($.state))
   })
+  storedData = JSON.parse(localStorage.getItem(STORAGE_KEY))
 }
+const $ = createStore(storedData || modelData)
 
 export default $
