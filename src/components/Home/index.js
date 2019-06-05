adb73601c0281ebda66dd2919545cb1ee7a548d1
ap4147 <alessandro.pungitore@amanninformatik.ch>
2019-06-05 16:05:36 +0200

    rfc
    Signed-off-by: ap4147 <alessandro.pungitore@amanninformatik.ch>

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,13 +3,14 @@ import _ from "./styles.css"
 import $ from "state"
 
 export default () => {
+  const onCount = () => $.update(s => (s.count += 1))
   const { count } = $.state
   return (
     <div className={_.box}>
-      <p>This text will be blueviolet.</p>
-      <i>store.count is: {count}</i>
+      <p>This text will be blueviolett. or not?</p>
+      <i>store ount is: {count}</i>
       <br />
-      <button onClick={() => $.update(s => (s.count += 1))}>count</button>
+      <button onClick={onCount}>count</button>
     </div>
   )
 }
